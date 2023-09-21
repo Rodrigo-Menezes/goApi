d4421d5d890be7276a72da115d420c89f909452b
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-09-21 15:46:45 -0300

    feat: add opening schemas

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "apiGo/router"
+import (
+	config "apiGo/Config"
+	"apiGo/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	// Initialize router
 	router.Initialize()
 }
